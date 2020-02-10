ef87a07c5134e31fae0d86dccd6a3c5624ae1880
elgunguluyev <elgun105@gmail.com>
2020-02-10 17:47:36 +0400

    added 2 files

---
+++ app.js
@@ -0,0 +1,68 @@
+
+let operator = '';
+let firstNumber = '';
+let secondNumber = '';
+let result = '';
+let lockTheKeyboard = true;
+
+
+$('.number').on('click', function(){
+    if(lockTheKeyboard == false){
+        return false;
+    }
+    if(!operator){
+        firstNumber += this.value;
+        $('#first-number').html(firstNumber);
+    }else {
+        secondNumber += this.value;
+        $('#second-number').html(secondNumber);
+    }
+});
+
+$('.operator').on('click', function(){
+
+    operator = this.value;
+    if(operator == 'plus'){
+        $('#operator').html('+');
+    }else if (operator == 'minus'){
+        $('#operator').html('-');
+    }else if( operator == 'times'){
+        $('#operator').html('*');
+    }else if(operator == 'power'){
+        $('#operator').html('^');
+    }else if(operator == 'divide'){
+        $('#operator').html('/');
+    }
+});
+
+$('.equal').on('click', function(){
+    if($('#operator').html() === '+'){
+        result = parseInt(firstNumber) + parseInt(secondNumber);
+        $('#result').html(result);
+    }else if($('#operator').html() === '-'){
+        result = parseInt(firstNumber) - parseInt(secondNumber);
+        $('#result').html(result);
+    }else if($('#operator').html() === '*'){
+        result = parseInt(firstNumber) * parseInt(secondNumber);
+        $('#result').html(result);
+    }else if($('#operator').html() === '^'){
+        result = parseInt(firstNumber) ** parseInt(secondNumber);
+        $('#result').html(result);
+    }else if($('#operator').html() === '/'){
+        result = parseInt(firstNumber) / parseInt(secondNumber);
+        $('#result').html(result);
+    }
+    lockTheKeyboard = false;
+});
+
+document.querySelector('.clear').addEventListener('click', function(){
+    firstNumber = '';
+    secondNumber = '';
+    operator = '';
+    result = '';
+    document.querySelector('#first-number').innerHTML= '';
+    document.querySelector('#second-number').innerHTML= '';
+    document.querySelector('#operator').innerHTML= '';
+    document.querySelector('#result').innerHTML= '';
+    lockTheKeyboard = true;
+});
